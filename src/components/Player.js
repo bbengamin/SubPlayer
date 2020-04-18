bae5f741d8ea2e1da47a7a9714e036c4c8482d53
Ihor Bohdanov <bbengamin@gmail.com>
2020-04-18 20:06:07 +0300

    Fix render flowplayer bug on video change

--- src/components/Player.js
+++ src/components/Player.js
@@ -34,86 +34,78 @@ function artPlayerActive() {
     aPlayer.seek = fPlayer.video.time
 }
 
-export default React.memo(
-    function({ options, setPlayer, setCurrentTime }) {
-        return (
-            <Player>
-                <ArtplayerComponent
-                    style={{
-                        width: '49%',
-                        height: '100%',
-                    }}
-                    option={{
-                        url: options.videoUrl,
-                        loop: false,
-                        autoSize: true,
-                        aspectRatio: true,
-                        playbackRate: true,
-                        fullscreen: true,
-                        fullscreenWeb: true,
-                        miniProgressBar: true,
-                        subtitle: {
-                            url: options.subtitleUrl,
-                        },
-                        moreVideoAttr: {
-                            crossOrigin: 'anonymous',
-                            preload: 'auto',
-                        },
-                    }}
-                    getInstance={art => {
-                        setPlayer(art);
-                        aPlayer = art;
+export default function({ options, setPlayer, setCurrentTime }) {
+    return (
+        <Player>
+            <ArtplayerComponent
+                style={{
+                    width: '49%',
+                    height: '100%',
+                }}
+                option={{
+                    url: options.videoUrl,
+                    loop: false,
+                    autoSize: true,
+                    aspectRatio: true,
+                    playbackRate: true,
+                    fullscreen: true,
+                    fullscreenWeb: true,
+                    miniProgressBar: true,
+                    subtitle: {
+                        url: options.subtitleUrl,
+                    },
+                    moreVideoAttr: {
+                        crossOrigin: 'anonymous',
+                        preload: 'auto',
+                    },
+                }}
+                getInstance={art => {
+                    setPlayer(art);
+                    aPlayer = art;
 
-                        (function loop() {
-                            window.requestAnimationFrame(() => {
-                                if (art.playing) {
-                                    setCurrentTime(art.currentTime);
-                                }
-                                loop();
-                            });
-                        })();
-
-                        art.on('seek', () => {
-                          setCurrentTime(art.currentTime);
+                    (function loop() {
+                        window.requestAnimationFrame(() => {
+                            if (art.playing) {
+                                setCurrentTime(art.currentTime);
+                            }
+                            loop();
                         });
+                    })();
 
-                        art.on('play', () => {
-                          artPlayerActive()
-                        });
+                    art.on('seek', () => {
+                        setCurrentTime(art.currentTime);
+                    });
 
-                        art.on('video:canplay', function(event) {
-                          setTimeout(()=> {
-                            let flowPlayerVideo = document.querySelector("#reactFlowPlayer video")
-                            if (flowPlayerVideo)
-                              flowPlayerVideo.setAttribute('src', event.target.src);
-                          }, 500)
-                        });
-                    }}
-                />
-                <ReactFlowPlayer
-                    playerInitScript="http://releases.flowplayer.org/7.2.1/flowplayer.min.js"
-                    playerId="reactFlowPlayer"
-                    sources={[
-                        {
-                          type: "video/webm",
-                          src: options.videoUrl
-                        }
-                    ]}
-                    video={{
+                    art.on('play', () => {
+                        artPlayerActive()
+                    });
+                }}
+            />
+            <ReactFlowPlayer
+                playerInitScript="http://releases.flowplayer.org/7.2.1/flowplayer.min.js"
+                playerId="reactFlowPlayer"
+                key={options.videoUrl}
+                sources={[
+                    {
+                        type: "video/webm",
                         src: options.videoUrl
-                    }}
-                    speedPlugin={ true }
-                    speeds={[1, 5, 10]}
-                    onResume={() => { flowPlayerActive() }}
-                    getPlayerApi={player => {
-                        fPlayer = player
-                        fPlayer.on('pause', () => {
-                            artPlayerActive()
-                        });
-                    }}
-                />
-            </Player>
-        );
-    },
-    () => true,
-);
+                    }
+                ]}
+                video={{
+                    src: options.videoUrl
+                }}
+                speedPlugin={true}
+                speeds={[1, 5, 10]}
+                onResume={() => {
+                    flowPlayerActive()
+                }}
+                getPlayerApi={player => {
+                    fPlayer = player
+                    fPlayer.on('pause', () => {
+                        artPlayerActive()
+                    });
+                }}
+            />
+        </Player>
+    );
+}
